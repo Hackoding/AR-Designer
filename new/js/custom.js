82d6dfa468fbd8beec88b99f467e52e8555a2f4f
AR Designer <64784051+Hackoding@users.noreply.github.com>
2020-08-15 06:31:45 +0530

    df

--- new/js/custom.js
+++ new/js/custom.js
@@ -35,8 +35,43 @@ if ($(window).width() < 991) {
 
 
 
+$(document).ready(function () {
+    //Disable full page
+    $('body').bind('cut copy paste', function (e) {
+        e.preventDefault();
+    });
+    // Disable mouse right click
+    $("body").on("contextmenu",function(e){
+        return false;
+        
+    });
+     $(document).bind("contextmenu",function(e){
+       return false;
+         });
+     document.onkeydown = function(e) {
+             if (e.ctrlKey && 
+                 (e.keyCode === 67 || 
+                  e.keyCode === 86 || 
+                  e.keyCode === 85 || 
+                  e.keyCode === 117)) {
+                 // alert('not allowed');
+                 return false;
+             } else {
+                 return true;
+             }
+     };
+
   
+    
+
+    
+ });
+
+
+
+
   
+  // 
 
 // $(document).bind("contextmenu",function(e){
 //   return false;
